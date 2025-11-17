846154433262063a631950ea18ba9346a3ea3056
kentoverse <kentoverse@gmail.com>
2025-11-17 07:09:01 -0500

    kk

--- CavadaMarc_CA-PRTQS_A02/CavadaMarc_CA-PRTQS_A02_code/Q2/Models/ItemName.cs
+++
@@ -1,9 +0,0 @@
-namespace Q2_ItemsJoin.Models
-{
-    public class ItemName
-    {
-        public int Sno { get; set; }
-        public int Id { get; set; }
-        public string Iname { get; set; } = "";
-    }
-}
--- CavadaMarc_CA-PRTQS_A02/CavadaMarc_CA-PRTQS_A02_code/Q2/Models/ItemPrice.cs
+++
@@ -1,8 +0,0 @@
-namespace Q2_ItemsJoin.Models
-{
-    public class ItemPrice
-    {
-        public int Id { get; set; }
-        public decimal Price { get; set; }
-    }
-}
